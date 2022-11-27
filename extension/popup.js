657a616c887779b8a95cd8149183a8a6fe61fcd2
Hulu2007 <49009136+Hulu2007@users.noreply.github.com>
2022-11-26 21:24:58 -0600

    Hulu FrontEnd

--- extension/popup.js
+++ extension/popup.js
@@ -10,21 +10,52 @@ const mocked_data = [
   {
     text: 'beef',
   },
+  {
+    text: 'pork',
+  },
+  {
+    text: 'fish',
+  },
 ];
 
-console.log('this is from popup.js');
-
-async function testApi() {
-  fetch('http://127.0.0.1:5000/')
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      testH2.textContent = data.text;
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+/**
+ * 
+ * @param {Array} arr 
+ */
+ function sortList (arr) {
+  const counter = new Map()
+  arr.forEach(item => {
+    const count = (counter.get(item.text)?.count || 0) + 1
+    counter.set(item.text, {count, item})
+    
+  })
+  const mapArr = Array.from(counter.entries())
+  mapArr.sort((a, b) => b[1].count - a[1].count)
+  return mapArr.map(mappedItem => mappedItem[1].item)
 }
 
-testApi();
+const ulElement = document.createElement('ul')
+const sortedMockedData = sortList(mocked_data)
+sortedMockedData.forEach(dish => {
+  const li = document.createElement('li')
+  li.innerHTML = `<span>${dish.text}</span>`
+  ulElement.appendChild(li)
+})
+document.body.appendChild(ulElement)
+
+// console.log('this is from popup.js');
+
+// async function testApi() {
+//   fetch('http://127.0.0.1:5000/')
+//     .then((res) => {
+//       return res.json();
+//     })
+//     .then((data) => {
+//       testH2.textContent = data.text;
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//     });
+// }
+
+// testApi();
